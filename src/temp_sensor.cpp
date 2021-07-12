17450d7b389adde7246f22f761a16ca5b967b6ee
Elliot Schumacher <elliot.l.schumacher@gmail.com>
2021-07-12 16:37:01 -0700

    Config saved and read in binary

--- src/temp_sensor.cpp
+++ src/temp_sensor.cpp
@@ -6,10 +6,28 @@
 const uint8_t LED_ON = 0;
 const uint8_t LED_OFF = 1;
 const uint8_t LED_HB = LED_BUILTIN;
+const String CONFIG_PATH = "/config.bin";
+const uint8_t GUID_LENGTH = 8;
 
 void manageBlink(uint32_t msNow, uint16_t timeOn, uint16_t timeOff);
-void readFile(String filePath);
+void setupConfigVariables();
+void printConfigVariables();
+void writeConfigFile();
+void printFile(String filePath);
 
+struct Node{
+    char guid[GUID_LENGTH];
+    uint32_t tempCheckInterval;
+    uint32_t errorInterval;
+    uint8_t sensorCount;
+};
+
+struct Sensor {
+    byte address[8];
+};
+
+Node nodeData;
+Sensor sensors[8];
 DelayTimer dtBlink;
 
 void setup() {
@@ -21,7 +39,10 @@ void setup() {
         Serial.println("An Error has occurred while mounting LittleFS");
         return;
     }
-    readFile("/config.txt");
+    writeConfigFile();
+    setupConfigVariables();
+    printConfigVariables();
+    printFile(CONFIG_PATH);
 }
 
 void loop() {
@@ -41,16 +62,75 @@ void manageBlink(uint32_t msNow, uint16_t timeOn, uint16_t timeOff) {
     }   
 }
 
-void readFile(String filePath) {
-    File file = LittleFS.open(filePath, "r");
-    if(!file){
+void setupConfigVariables() {
+    File file = LittleFS.open(CONFIG_PATH, "r");
+    if (!file) {
         Serial.println("Failed to open file for reading");
-        return;
+    } else {
+        file.read((byte *)&nodeData, sizeof(nodeData));
+        for (uint8_t sensorIndex = 0; sensorIndex < nodeData.sensorCount; sensorIndex++) {
+            file.read((byte *)&sensors[sensorIndex], sizeof(Sensor));
+        }
+        file.close();
     }
+}
 
-    Serial.println("File Content:");
-    while(file.available()){
-        Serial.write(file.read());
+void printConfigVariables() {
+    Serial.println("nodeGuid:");
+    for (uint8_t index = 0; index < sizeof(nodeData.guid); index++) {
+        Serial.print(nodeData.guid[index]);
     }
-    file.close();
+    Serial.println();
+    Serial.println("tempCheckInterval:");
+    Serial.println(nodeData.tempCheckInterval);
+    Serial.println("errorInterval:");
+    Serial.println(nodeData.errorInterval);
+    Serial.println("sensorCount:");
+    Serial.println(nodeData.sensorCount);
+    for (uint8_t sensorIndex = 0; sensorIndex < nodeData.sensorCount; sensorIndex++) {
+        Serial.print("sensorAddress:");
+        Serial.print(sensorIndex);
+        Serial.println(":");
+        for (int index = 0; index < 8; index++) {
+            Serial.print(sensors[sensorIndex].address[index], HEX);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
+}
+
+void writeConfigFile() {
+    const Node nodeDataToSave = {
+        {'n', 'o', 'd', 'e', 'G', 'U', 'I', 'D'},
+        30500,
+        45050,
+        2
+    };
+    const Sensor warmSensor = {
+        {0x28, 0xB3, 0x53, 0x07, 0xD6, 0x01, 0x3C, 0x0C}
+    };
+    const Sensor coolSensor = {
+        {0x28, 0x0D, 0x5B, 0x07, 0xD6, 0x01, 0x3C, 0x26}
+    };
+    File file = LittleFS.open(CONFIG_PATH, "w+");
+    if (!file) {
+        Serial.println("Failed to open file for writing");
+    } else {
+        file.write((byte *)&nodeDataToSave, sizeof(nodeDataToSave));
+        file.write((byte *)&warmSensor, sizeof(warmSensor));
+        file.write((byte *)&coolSensor, sizeof(coolSensor));
+        file.close();
+    }
+}
+
+void printFile(String filePath) {
+    File file = LittleFS.open(filePath, "r");
+    if (!file) {
+        Serial.println("Failed to open file for printing");
+    } else {
+        while (file.available()) {
+            Serial.print(file.read(), BIN);
+        }
+        file.close();
+    }
 }
